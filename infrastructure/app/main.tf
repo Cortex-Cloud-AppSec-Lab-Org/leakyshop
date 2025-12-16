f7b90af62bd4c03f90753e268cb4e454bd9ca28f
Cortex-Cloud-Lab <sifuentesemanuel@gmail.com>
2025-12-16 17:11:23 -0500

    leakyshop-branch

--- infrastructure/app/main.tf
+++ infrastructure/app/main.tf
@@ -63,18 +63,29 @@ resource "aws_route_table_association" "b" {
 resource "aws_security_group" "allow_all" {
   name        = "allow_all_traffic"
   vpc_id      = aws_vpc.leaky_vpc.id
+
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+    # RDP
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
